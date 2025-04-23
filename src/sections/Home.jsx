fe033f62864ba20313d1a328b9125d9554e3cb0e
yashiikaaaa <yashika@truestate.in>
2025-04-23 17:04:27 +0530

    location

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -69,7 +69,7 @@ export const Home = ({ contactmodal, setContactModal }) => {
               className="h-3 md:h-6 text-black"
             />
             <p className="max-w-96 font-body text-left text-black md:text-2xl text-xs font-medium leading-[130%]">
-              Sadahalli {/* Location name */}
+              Begur Lakefront {/* Location name */}
             </p>
           </div>
         </div>
